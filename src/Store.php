24809013a3f09ce4142e9b7f9308fa7648da8104
Dana Sharman <cconeus@gmail.com>
2015-08-28 10:39:43 -0700

    show 8 passing tests for Store class

---
+++ src/Store.php
@@ -0,0 +1,66 @@
+<?php
+
+    class Store
+    {
+        private $id;
+        private $store;
+
+        //construct, getters and setters
+        function __construct($id = null, $store)
+        {
+            $this->id = $id;
+            $this->store = $store;
+        }
+
+        function setStore($new_store)
+        {
+            $this->store = $new_store;
+        }
+
+        function getStore()
+        {
+            return $this->store;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+        //CRUD functions for save, delete, update
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stores (store) VALUES ('{$this->getStore()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        function update($new_store)
+        {
+            $GLOBALS['DB']->exec("UPDATE stores SET store = '{$new_store}' WHERE id = {$this->getId()};");
+            $this->setStore($new_store);
+        }
+
+        function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stores WHERE id = {$this->getId()};");
+        }
+
+        //static functions
+        static function getAll()
+        {
+            $show_stores = $GLOBALS['DB']->query("SELECT * FROM stores;");
+            $stores = array();
+            foreach($show_stores as $store) {
+                $id = $store['id'];
+                $name = $store['store'];
+                $new_store = new Store($id, $name);
+                array_push($stores, $new_store);
+            }
+        return $stores;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stores;");
+        }
+    }
+ ?>
---
+++ tests/StoreTest.php
@@ -0,0 +1,147 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Store.php";
+    require_once "src/Brand.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=shoes_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    class StoreTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+            Store::deleteAll();
+        }
+
+        function testGetStore()
+        {
+            //Arrange
+            $id = 1;
+            $store = "Payless Shoes";
+            $test_store = new Store($id, $store);
+
+            //Act
+            $result = $test_store->getStore();
+
+            //Assert
+            $this->assertEquals($store, $result);
+        }
+
+        function testSetStore()
+        {
+            $id = 1;
+            $store = "Payless Shoes";
+            $test_store = new Store($id, $store);
+
+            $test_store->setStore("JC Penny");
+            $result = $test_store->getStore();
+
+            $this->assertEquals("JC Penny", $result);
+        }
+
+        function testGetId()
+        {
+            $id = 1;
+            $store = "Payless Shoes";
+            $test_store = new Store($id, $store);
+
+            $result = $test_store->getId();
+
+            $this->assertEquals(1, $result);
+        }
+
+        function testSave()
+        {
+            //Arrange
+            $id = 1;
+            $store = "Payless Shoes";
+            $test_store = new Store($id, $store);
+            $test_store->save();
+
+            //Act
+            $result = Store::getAll();
+
+            //Assert
+            $this->assertEquals($test_store, $result[0]);
+        }
+
+        function testUpdate()
+        {
+            $id = 1;
+            $store = "Payless Shoes";
+            $test_store = new Store($id, $store);
+            $test_store->save();
+
+            $new_store = "Meyer & Franks";
+
+            $test_store->update($new_store);
+
+            $this->assertEquals("Meyer & Franks", $test_store->getStore());
+        }
+
+        function testDelete()
+        {
+            $id = 1;
+            $store = "Payless Shoes";
+            $test_store = new Store($id, $store);
+            $test_store->save();
+
+            $id2 = 2;
+            $store2 = "Meyer & Frank";
+            $test_store2 = new Store($id2, $store2);
+            $test_store2->save();
+
+            $test_store->delete();
+
+            $this->assertEquals([$test_store2], Store::getAll());
+        }
+
+        function testGetAll()
+        {
+            //Arrange
+            $id = 1;
+            $store = "Payless Shoes";
+            $test_store = new Store($id, $store);
+            $test_store->save();
+
+            $id2 = 2;
+            $store2 = "Meyer & Frank";
+            $test_store2 = new Store($id2, $store2);
+            $test_store2->save();
+
+            //Act
+            $result = Store::getAll();
+
+            //Assert
+            $this->assertEquals([$test_store, $test_store2], $result);
+        }
+
+        function testDeleteAll()
+        {
+            //Arrange
+            $id = 1;
+            $store = "Payless Shoes";
+            $test_store = new Store($id, $store);
+            $test_store->save();
+
+            $id2 = 2;
+            $store2 = "Meyer & Frank";
+            $test_store2 = new Store($id, $store);
+            $test_store2->save();
+
+            Store::deleteAll();
+
+            //Assert
+            $result = Store::getAll();
+            $this->assertEquals([], $result);
+        }
+    }
+ ?>
